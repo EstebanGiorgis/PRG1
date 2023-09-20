c1050526d2d7955f611e3fc28509c305c692f99c
EstebanGiorgis <estebangiorgisg@gmail.com>
2023-09-20 14:42:41 +0200

    Premier projet HelloWorld

---
+++ Projet_CLion/Exercice_HelloWorld/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+//main function is the first function called at the beginning of the program
+int main() {
+    cout << "Hello, World!" << endl;//cout->consol out
+    return EXIT_SUCCESS;
+} 
