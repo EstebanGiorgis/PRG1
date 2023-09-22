692b0b0a6af62619b28a079057bf34b07b597fb6
EstebanGiorgis <estebangiorgisg@gmail.com>
2023-09-22 09:04:48 +0200

    fin de l'exercice UncaughtException

--- Projet_CLion/Exercice_HelloWorld/main.cpp
+++ Projet_CLion/Exercice_HelloWorld/main.cpp
@@ -5,5 +5,6 @@ using namespace std;
 //main function is the first function called at the beginning of the program
 int main() {
     cout << "Hello, World!" << endl;//cout->consol out
+
     return EXIT_SUCCESS;
 } 
